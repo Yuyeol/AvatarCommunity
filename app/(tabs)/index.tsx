d767fcf7537c09458e03e293619bd8aa940869c8
Yuyeol <uy23@naver.com>
2025-02-21 13:36:43 +0900

    feat #1: 인증 화면 구성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -6,7 +7,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
-      <CustomButton label="확인" onPress={() => console.log("확인")} />
+      <CustomButton label="로그인" onPress={() => router.push("/auth")} />
     </SafeAreaView>
   );
 }
