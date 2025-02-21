a6ff2702aec62e673243fb864c8853635253ed66
Yuyeol <uy23@naver.com>
2025-02-21 12:55:01 +0900

    feat #1: 탭 구성

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
+import { colors } from "@/constants";
 import { Tabs } from "expo-router";
-
+import { Ionicons } from "@expo/vector-icons";
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "black",
+        tabBarActiveTintColor: colors.ORANGE_600,
         headerShown: false,
       }}
     >
@@ -12,18 +13,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "홈",
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "탐색",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "내 정보",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/setting/index.tsx
+++ app/(tabs)/setting/index.tsx
@@ -1,10 +1,10 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function ExploreScreen() {
+export default function SettingScreen() {
   return (
     <SafeAreaView>
-      <Text>탐색</Text>
+      <Text>설정</Text>
     </SafeAreaView>
   );
 }
