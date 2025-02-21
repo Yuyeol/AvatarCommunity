fd2f0c9e70c157ea40bd30f22259ac04873fbb9d
Yuyeol <uy23@naver.com>
2025-02-21 15:42:50 +0900

    feat #1: feedItem, feedList 구성 및 data type 추가

---
+++ components/FeedList.tsx
@@ -0,0 +1,64 @@
+import { Post } from "@/app/types";
+import FeedItem from "@/components/FeedItem";
+import { colors } from "@/constants";
+import { FlatList, StyleSheet } from "react-native";
+
+const dummyData: Post[] = [
+  {
+    id: 1,
+    userId: 1,
+    title: "title",
+    description:
+      "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "nickname",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "title",
+    description:
+      "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "nickname",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+];
+
+export default function FeedList() {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY_200,
+    gap: 12,
+  },
+});
