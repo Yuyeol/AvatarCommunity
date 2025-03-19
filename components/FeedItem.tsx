d016611906f7509168eb8d8e0884b59454399f02
ure <uy23@naver.com>
2025-03-19 16:17:56 +0900

    feat #1: 좋아요 기능 추가

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -12,8 +12,9 @@ import useAuth from "@/hooks/queries/useAuth";
 import { useActionSheet } from "@expo/react-native-action-sheet";
 import useDeletePost from "@/hooks/queries/useDeletePost";
 import { router } from "expo-router";
-import ImagePreviewList from "./ImagePreviewList";
-import Vote from "./Vote";
+import ImagePreviewList from "@/components/ImagePreviewList";
+import Vote from "@/components/Vote";
+import useLikePost from "@/hooks/queries/useLikePost";
 
 interface FeedItemProps {
   post: Post;
@@ -26,6 +27,7 @@ export default function FeedItem({ post, isDetail = false }: FeedItemProps) {
   const isLiked = likedUser?.includes(Number(auth.id));
   const { showActionSheetWithOptions } = useActionSheet();
   const deletePost = useDeletePost();
+  const likePost = useLikePost();
   const handlePressOption = () => {
     const options = ["삭제", "수정", "취소"];
     const destructiveButtonIndex = 0;
@@ -61,6 +63,18 @@ export default function FeedItem({ post, isDetail = false }: FeedItemProps) {
       router.push(`/post/${post.id}`);
     }
   };
+
+  const handlePressLike = () => {
+    if (!auth.id) {
+      router.push("/auth");
+      return;
+    }
+    if (!isDetail) {
+      router.push(`/post/${post.id}`);
+    }
+    likePost.mutate(post.id);
+  };
+
   const ContainerComponent = isDetail ? View : Pressable;
 
   return (
@@ -111,7 +125,7 @@ export default function FeedItem({ post, isDetail = false }: FeedItemProps) {
         )}
       </View>
       <View style={styles.menuContainer}>
-        <Pressable style={styles.menu}>
+        <Pressable style={styles.menu} onPress={handlePressLike}>
           <Octicons
             name={isLiked ? "heart-fill" : "heart"}
             size={16}
@@ -121,7 +135,7 @@ export default function FeedItem({ post, isDetail = false }: FeedItemProps) {
             {post.likes.length || "좋아요"}
           </Text>
         </Pressable>
-        <Pressable style={styles.menu}>
+        <Pressable style={styles.menu} onPress={handlePressFeed}>
           <MaterialCommunityIcons
             name="comment-processing-outline"
             size={16}
@@ -129,7 +143,7 @@ export default function FeedItem({ post, isDetail = false }: FeedItemProps) {
           />
           <Text style={styles.menuText}>{post.commentCount || "댓글"}</Text>
         </Pressable>
-        <Pressable style={styles.menu}>
+        <Pressable style={styles.menu} onPress={handlePressFeed}>
           <Ionicons name="eye-outline" size={16} color={colors.BLACK} />
           <Text style={styles.menuText}>{post.viewCount}</Text>
         </Pressable>
--- hooks/queries/useUpdatePost.ts
+++ hooks/queries/useUpdatePost.ts
@@ -8,10 +8,10 @@ function useUpdatePost() {
   return useMutation({
     mutationFn: updatePost,
     onSuccess: (postId) => {
-      // 수정한 게시글 무효화. 이건 모든게시글 무효화하면 필요없을듯? 일단 주석처리
-      //   queryClient.invalidateQueries({
-      //     queryKey: [queryKeys.POST, queryKeys.GET_POST, postId],
-      //   });
+      // 수정한 게시글 무효화
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POST, postId],
+      });
       //   모든 게시글 무효화
       queryClient.invalidateQueries({
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
